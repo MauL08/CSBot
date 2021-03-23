328fd69af025e73dac10dd9d5d45549b644fe89e
MosQyy <akbarma2002@gmail.com>
2021-03-23 07:45:51 +0700

    upload server

--- src/commands/searchCommand.js
+++ src/commands/searchCommand.js
@@ -27,88 +27,88 @@ module.exports = {
         }
         if (command === 'search') {
             switch(args[0]) {
-                case 'image':
-                    (async () => {
+                    case 'image':
+                        (async () => {
+                            args.splice(0,1);
+                            const imageQueue = args.join(' ');
+                            const imgRes = await google.scrape(imageQueue, 100);
+                            message.channel.send(imgRes[Math.floor(Math.random() * imgRes.length)].url)
+                        })();
+                    break;
+                    case 'google':
                         args.splice(0,1);
-                        const imageQueue = args.join(' ');
-                        const imgRes = await google.scrape(imageQueue, 10);
-                        message.channel.send(imgRes[Math.floor(Math.random() * imgRes.length)].url)
-                    })();
-                break;
-                case 'google':
-                    args.splice(0,1);
-                    (async () => {
-                        const googleKey = process.env.GOOGLE_API;
-                        const csx = 'b4340115fcd24ce3c'
-                        let searchQueue = args.join(' ');
-    
-                        if (!searchQueue) return message.channel.send('Please Input search Subject.');
-    
-                        href = await search(searchQueue);
-                        if (!href) return message.channel.send('Nothing Found!')
-    
-                        const emb = new MessageEmbed()
-                            .setTitle(href.title)
-                            .setDescription(href.snippet)
-                            .setImage(href.pagemap ? href.pagemap.cse_thumbnail[0].src : null)
-                            .setURL(href.link)
-                            .setColor('RANDOM')
-                            .setFooter('Powered by Google')
-                        message.channel.send(emb)
-    
-                        async function search(query) {
-                            const { body } = await request.get('https://www.googleapis.com/customsearch/v1').query({
-                                key: googleKey, cx: csx, safe: 'off', q: query 
-                            });
-    
-                            if (!body.items) return null;
-                            return body.items[0];
-                        }
-                    })();
-                break;
-                case 'wiki':
-                    args.splice(0,1);
-                    (async () => {
-                        const subWiki = args.join(' ');
-                        if (!subWiki) message.channel.send('Please type the correct Subject.')
-                        
-                        const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(subWiki)}`
-    
-                        let response;
-    
-                        try {
-                            response = await fetch(url).then(res => res.json())
-                        }      
-                        catch (e) {
-                            return message.channel.send('An Error Occured, Try Again.')
-                        }
-    
-                        try {
-                            if(response.type === 'disambiguation') {
-                                const embed = new MessageEmbed()
-                                    .setColor('RANDOM')
-                                    .setTitle(response.title)
-                                    .setURL(response.content_urls.desktop.page)
-                                    .setDescription([`
-                                    ${response.extract}
-                                    Links For Topic You Searched [Link](${response.content_urls.desktop.page}).`])
-                                message.channel.send(embed)
+                        (async () => {
+                            const googleKey = process.env.GOOGLE_API;
+                            const csx = 'b4340115fcd24ce3c'
+                            let searchQueue = args.join(' ');
+        
+                            if (!searchQueue) return message.channel.send('Please Input search Subject.');
+        
+                            href = await search(searchQueue);
+                            if (!href) return message.channel.send('Nothing Found!')
+        
+                            const emb = new MessageEmbed()
+                                .setTitle(href.title)
+                                .setDescription(href.snippet)
+                                .setImage(href.pagemap ? href.pagemap.cse_thumbnail[0].src : null)
+                                .setURL(href.link)
+                                .setColor('RANDOM')
+                                .setFooter('Powered by Google')
+                            message.channel.send(emb)
+        
+                            async function search(query) {
+                                const { body } = await request.get('https://www.googleapis.com/customsearch/v1').query({
+                                    key: googleKey, cx: csx, safe: 'off', q: query 
+                                });
+        
+                                if (!body.items) return null;
+                                return body.items[0];
                             }
-                            else {
-                                const embed = new MessageEmbed()
-                                    .setColor('RANDOM')
-                                    .setTitle(response.title)
-                                    .setThumbnail(response.thumbnail.source)
-                                    .setURL(response.content_urls.desktop.page)
-                                    .setDescription(response.extract)
-                                message.channel.send(embed)
+                        })();
+                    break;
+                    case 'wiki':
+                        args.splice(0,1);
+                        (async () => {
+                            const subWiki = args.join(' ');
+                            if (!subWiki) message.channel.send('Please type the correct Subject.')
+                            
+                            const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(subWiki)}`
+        
+                            let response;
+        
+                            try {
+                                response = await fetch(url).then(res => res.json())
+                            }      
+                            catch (e) {
+                                return message.channel.send('An Error Occured, Try Again.')
                             }
-                        }
-                        catch {
-                            return message.channel.send('Your input is not in Wikipedia, try another input!')
-                        }
-                    })();
-                break;
+        
+                            try {
+                                if(response.type === 'disambiguation') {
+                                    const embed = new MessageEmbed()
+                                        .setColor('RANDOM')
+                                        .setTitle(response.title)
+                                        .setURL(response.content_urls.desktop.page)
+                                        .setDescription([`
+                                        ${response.extract}
+                                        Links For Topic You Searched [Link](${response.content_urls.desktop.page}).`])
+                                    message.channel.send(embed)
+                                }
+                                else {
+                                    const embed = new MessageEmbed()
+                                        .setColor('RANDOM')
+                                        .setTitle(response.title)
+                                        .setThumbnail(response.thumbnail.source)
+                                        .setURL(response.content_urls.desktop.page)
+                                        .setDescription(response.extract)
+                                    message.channel.send(embed)
+                                }
+                            }
+                            catch {
+                                return message.channel.send('Your input is not in Wikipedia, try another input!')
+                            }
+                        })();
+                    break;
                 }
             }        
         }
